b3f707dfb1d56ed7616e924aeba99ce4045fff62
jaapp <github@pieroen.nl>
2014-01-08 14:05:27 +0100

    Meer commentaar

--- update_dns_records.py
+++ update_dns_records.py
@@ -6,37 +6,41 @@ from selenium.webdriver.common.keys import Keys
 import time
 import urllib2
 
+# Configfile inlezen
 config = {}
-execfile("dnsrecords.config", config) 
+execfile("dnsrecords.config", config)
 
+# Variabelen initialiseren
 domains  = config['domains']
 username = config['username']
 password = config['password']
 cnt = 0
 
+# Procedure om screenshots op te slaan van de headless browsert. Voegt een volgnummer toe aan de bestandsnaam
 def screenshot(descr):
 	global cnt
 	global driver
 	cnt = cnt + 1
 	driver.save_screenshot(str(cnt).zfill(3) + '_' + descr + '.png')
 
-# IP Ophalen	
+# Extern IP Ophalen	
 myip = urllib2.urlopen("http://myip.dnsdynamic.org/").read()
 	
 # Browser starten
 driver = webdriver.PhantomJS()
 
-# Inloggen
+# Inloggen op mijndomein.nl
 driver.get("http://www.mijndomein.nl/login")
 driver.find_element_by_name("loginform_username").send_keys(username)
 driver.find_element_by_name("loginform_password").send_keys(password) #, Keys.RETURN)
 driver.find_element_by_name("loginform_password").submit()
 screenshot('na_log-in')
 
+# Voor elk domein in het configuratiebestand:
 for domain in domains:
-	# DNS Instellen scherm openen
+	# 'DNS Instellen' pagina openen
 	a = driver.execute_script("puntapi.DoCommand('dnsclient','dnsadmin','','','');")
-	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "Formid")))
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "Formid"))) # Wacht tot de pagina is geladen
 	screenshot('na_dns_instellen_klik_' + domain['name'])
 	
 	# Domein kiezen
@@ -45,9 +49,10 @@ for domain in domains:
 	elem.send_keys(domain['name'])
 	time.sleep(1) # onchange event een kans geven
 	elem.send_keys(Keys.RETURN)
-	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "Formid")))
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "Formid"))) # Wacht tot de pagina is geladen
 	screenshot('na_domein_keuze_' + domain['name'])
 
+	# Voor elk subdomein:
 	for subdomain in domain['subdomains']:
 		# Klikken op naam subdomein
 		driver.find_element_by_xpath("//div[text()='" + subdomain + "']").click()
@@ -66,7 +71,7 @@ for domain in domains:
 
 	# Klikken op opslaan
 	driver.find_element_by_xpath("//td[text()='Opslaan']/ancestor::div[1]").click()
-	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[text()='DNS opgeslagen']")))
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[text()='DNS opgeslagen']"))) # Wacht op confirmatie
 	screenshot('na_klikken_opslaan_knop_' + domain['name'])
 	
 driver.quit
